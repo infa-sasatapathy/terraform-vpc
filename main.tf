ddb00489a7dca35c4727b45555278fec6caf7dcb
Saumik Satapathy <oracle.saumik@gmail.com>
2025-10-03 18:20:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,14 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_name       = "production"
-  vpc_cidr       = var.vpc_cidr
-  eip_private_ip = "192.168.0.5"
+  region                = var.region
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_1_cidr  = var.public_subnet_1_cidr
+  public_subnet_2_cidr  = var.public_subnet_2_cidr
+  public_subnet_3_cidr  = var.public_subnet_3_cidr
+  private_subnet_1_cidr = var.private_subnet_1_cidr
+  private_subnet_2_cidr = var.private_subnet_2_cidr
+  private_subnet_3_cidr = var.private_subnet_3_cidr
 
-  public_subnets = {
-    pub1 = { cidr = var.public_subnet_1_cidr, az = "ap-south-1a" }
-    pub2 = { cidr = var.public_subnet_2_cidr, az = "ap-south-1b" }
-    pub3 = { cidr = var.public_subnet_3_cidr, az = "ap-south-1c" }
-  }
-
-  private_subnets = {
-    priv1 = { cidr = var.private_subnet_1_cidr, az = "ap-south-1a" }
-    priv2 = { cidr = var.private_subnet_2_cidr, az = "ap-south-1b" }
-    priv3 = { cidr = var.private_subnet_3_cidr, az = "ap-south-1c" }
-  }
+  environment = var.environment
 }
